81147624a1a34127b2fefdde312a5a889c8336c2
Frankie Leung <frankieleung1@hotmail.com>
2013-02-11 19:11:55 -0500

    got rid of the spam

--- game/Bullet.lua
+++ game/Bullet.lua
@@ -10,7 +10,6 @@ end,
 name = "Bullet", inherits = Entity})
 
 function Bullet:draw()
-	print("Bullet:draw()", self.pos.x, self.pos.y)
 	love.graphics.setColor(self.data.colour[1], self.data.colour[2], self.data.colour[3], self.data.colour[4] or 255)
 	love.graphics.rectangle("fill", self.pos.x, self.pos.y, 10, 10)
 end
